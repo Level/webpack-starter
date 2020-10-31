2f542593a29128bb6af4b77656f81fe342ff132f
Fergus McDowall <fergusmcdowall@gmail.com>
2020-10-31 22:59:38 +0100

    Revert "works on commandline and in browser"
    This reverts commit 86506312e3231e033e8dba45b3b14b2abf9bfda2.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import level from 'level'
 
-const main = async () => {
+async function main () {
   const db = level('webpack-starter')
   const output = document.getElementById('output')
+
   await db.put('beep', 'boop')
   output.textContent = await db.get('beep')
 }
--- src/web-bundle-console-log.js
+++
@@ -1,9 +0,0 @@
-import level from 'level'
-
-const main = async () => {
-  const db = level('webpack-starter')
-  await db.put('beep', 'boop')
-  return console.log(await db.get('beep'))
-}
-
-main()
